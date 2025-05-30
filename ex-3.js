c86f06ff67a8e013228dbd58544778bd77d047ee
kelia01 <iradukundakelia@gmail.com>
2025-05-30 14:01:18 +0200

    Async/ await basic err handling

---
+++ ex-3.js
@@ -0,0 +1,26 @@
+async function getUserData(userId) {
+    try {
+        const user = await fetch('https://jsonplaceholder.typicode.com/users/1');
+
+        if (!user.ok) {
+            throw new Error(`HTTP error! Status: ${user.status}`);
+        }
+        let userData;
+        try {
+            userData = await user.json();
+        } catch (err) {
+            throw new Error('Failed to parse JSON' + err.message);
+        }
+
+        if (!userData || typeof userData.name !== 'string') {
+            console.error('User data missing or does not have a name prop');
+        } else {
+            console.log(userData.name.toUpperCase());
+        }
+        return userData;
+    } catch (newErr) {
+        console.error('Was not able to deliver because of internet');
+    }
+}
+
+getUserData('2')
