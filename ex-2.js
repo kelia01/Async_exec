4a861004fe176cc448f511d1f2cd965ece6bc679
kelia01 <iradukundakelia@gmail.com>
2025-06-09 10:32:25 +0200

    Uncomment the callback code

--- ex-2.js
+++ ex-2.js
@@ -1,17 +1,17 @@
 
 
-// function fetchUserCallback(userId, callback) {
-//     setTimeout(() => {
-//       if (userId > 0) {
-//         callback(null, { id: userId, name: 'John' });
-//       } else {
-//         callback(new Error('Invalid user ID'));
-//       }
-//     }, 1000);
-//   }
-  
+function fetchUserCallback(userId, callback) {
+    setTimeout(() => {
+      if (userId > 0) {
+        callback(null, { id: userId, name: 'John' });
+      } else {
+        callback(new Error('Invalid user ID'));
+      }
+    }, 1000);
+  }
+
   // Convert to Promises:
-  
+
   function fetchUserPromise(userId) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -19,12 +19,11 @@
                 resolve({id: userId, name: 'John'});
             }
                 reject(new Error('Invalid user ID'))
-           
+
         }, 1000)
     })
   }
-  
+
   // Should work like:
    fetchUserPromise(1).then(user => console.log(user));
    fetchUserPromise(-1).catch(err => console.log(err.message));
-  
